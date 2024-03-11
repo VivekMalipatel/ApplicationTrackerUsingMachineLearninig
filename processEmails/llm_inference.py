774f5a6fcc67d5cc566c30f4abb165ce2a029c2f
Vivekanand Reddy Malipatel <77662002+VivekMalipatel@users.noreply.github.com>
2024-03-11 03:53:18 +0000

    commit

--- processEmails/llm_inference.py
+++ processEmails/llm_inference.py
@@ -11,7 +11,7 @@ class Config:
     "Applied": "The email is related to a job application that the recipient has submitted, for instance, a confirmation email received after applying for a job.",
     "Rejected": "The email is related to a rejection from a job application, indicating that the recipient was not selected for the job role or that the application will not be moving forward.",
     "Irrelevant": "The email is not related to job applications, such as applying, being rejected, or being accepted for a job role. It does not pertain to the status or process of job applications.",
-    "Accepted": "The email is related to the acceptance of a job application, indicating that the recipient has not just applied but been selected or accepted for a job role following an application."
+    "Accepted": "The email is related to the acceptance of a job application, indicating that the recipient has not just applied but got selected or accepted for a job position or a job role following an application."
     }
 
     hypothesis_company_name_dic = {}
@@ -29,8 +29,10 @@ class Model:
                                 framework="pt",
                                 device=Config.device,
                             )
+        self.predict()
     
-    def predict(self, text, hypothesis_class_label_dic=Config.hypothesis_class_label_dic, hypothesis_class_lst=Config.hypothesis_class_lst):
+    def predict(self, text =["The Subject : ""Ext Confirmation On The Position Of Software Developer Internship At Interactive Brokers LLC."" - End of the Subject The email: ""Dear Shoaib Mohammed We are pleased to extend the following offer of employment to you on behalf of Interactive Brokers LLC You have been selected a the best candidate for the Software Developer Internship position Congratulations We believe that your knowledge skill and experience would be an ideal fit for our IT department team We hope you will enjoy your role and make significant contribution to the overall success of Interactive Brokers LLC Please take the time to review our offer It includes important detail about your compensation benefit and the term and condition of your anticipated employment with Interactive Brokers LLC We will need all form signed and returned a soon a possible We are very excited to start this journey together and can wait to have you join the team You are expected to contact Cindy Via Trillian IM platform a regard further briefing on the position and Training Best Regard Recruiting Team"" -end of the email. "], hypothesis_class_label_dic=Config.hypothesis_class_label_dic, hypothesis_class_lst=Config.hypothesis_class_lst):
+
         pipe_output = self.pipe_classifier(
                         text,
                         candidate_labels=hypothesis_class_lst,
@@ -47,4 +49,7 @@ class Model:
         # map the long hypotheses to their corresponding short label names
         hypothesis_label_dic_inference_inverted = {value: key for key, value in hypothesis_class_label_dic.items()}
         label_pred = [hypothesis_label_dic_inference_inverted[hypo] for hypo in hypothesis_pred_true]
+        print(label_pred)
         return label_pred
+    
+#Model()
